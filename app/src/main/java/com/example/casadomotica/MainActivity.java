7022a1b68586a1a53f0066215bda300fdbc12fe0
CarmenCervantes <85425414+CarmenxCervantes@users.noreply.github.com>
2023-07-25 10:18:56 -0600

    Menú - Fragmentos

--- app/src/main/java/com/example/casadomotica/MainActivity.java
+++ app/src/main/java/com/example/casadomotica/MainActivity.java
@@ -2,13 +2,23 @@ package com.example.casadomotica;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements miMenu{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void menuboton(int boton) {
+        Intent in = new Intent(MainActivity.this, Estacionamiento.class);
+        in.putExtra("botonpul", boton);
+
+        //startActivities(new Intent[]{in});
+        startActivity(in);
     }
 }
---
+++ app/src/main/java/com/example/casadomotica/miMenu.java
@@ -0,0 +1,5 @@
+package com.example.casadomotica;
+
+public interface miMenu {
+    public void menuboton(int boton);
+}
